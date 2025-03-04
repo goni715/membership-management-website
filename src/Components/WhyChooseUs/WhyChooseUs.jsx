23d64fcf2ea4a1cebf834b5605f53e0f9a31642c
shukumar-542 <shukumar542@gmail.com>
2025-03-04 15:38:51 +0600

    Implement why choose us section

---
+++ src/Components/WhyChooseUs/WhyChooseUs.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import easy from '../../assets/images/easy.png'
+import Affiliate from '../../assets/images/Affiliate.png'
+import tracking from '../../assets/images/tracking.png'
+import reward from '../../assets/images/reward.png'
+
+const WhyChooseUs = () => {
+  return (
+    <div className='text-white mt-10'>
+        <p className='text-center text-[48px] font-bold'>Why Choose Us?</p>
+        <p className='text-center mt-5'>Unlock a world of opportunities with our referral program! Whether you’re looking to earn extra income or build a growing </p>
+        <p className='text-center'>network, our multi-level commission structure ensures you get rewarded at every step.</p>
+
+        <div className='md:flex justify-between items-center  py-10 mt-20'>
+            <div >
+                <img src={easy} alt="" />
+                <p className='text-xl font-semibold my-3'>Easy & Free Sign-Up</p>
+                <p>Get started in minutes with</p>
+                <p>your unique referral code</p>
+            </div>
+            <div >
+                <img src={Affiliate} alt="" />
+                <p className='text-xl font-semibold my-3'>Multi-Level Earnings</p>
+                <p>Earn commissions not just from </p>
+                <p>direct referrals but from their </p>
+                <p>referrals too!</p>
+            </div>
+            <div >
+                <img src={tracking} alt="" />
+                <p className='text-xl font-semibold my-3'>Seamless Tracking</p>
+                <p>Monitor your referrals and  </p>
+                <p> earnings effortlessly through </p>
+                <p>your dashboard.</p>
+            </div>
+            <div className=''>
+                <img src={reward} alt="" />
+                <p className='text-xl font-semibold my-3'>Instant Rewards</p>
+                <p>Get paid quickly and </p>
+                <p>securely as your network </p>
+                <p>expands.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default WhyChooseUs
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../../Components/Hero/Hero'
+import WhyChooseUs from '../../Components/WhyChooseUs/WhyChooseUs'
 
 const HomePage = () => {
   return (
     <div className='bg-[#090909]'>
         <div className='container mx-auto'>
             <Hero/>
+            <WhyChooseUs/>
         </div>
     </div>
   )
