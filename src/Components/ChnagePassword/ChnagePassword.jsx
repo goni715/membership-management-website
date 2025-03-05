9b6f0399bf985e66d14d6b9d85c6ea1afea2e829
shukumar-542 <shukumar542@gmail.com>
2025-03-05 12:11:12 +0600

    Implement chanage passoword page

--- src/Components/ChnagePassword/ChnagePassword.jsx
+++ src/Components/ChnagePassword/ChnagePassword.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Form, Input } from "antd";
+import React from "react";
 
 const ChnagePassword = () => {
   return (
-    <div>ChnagePassword</div>
-  )
-}
+    <div className="p-4">
+      <p className="text-xl font-bold">Change Password</p>
+      <div className="px-20 mt-10">
+        <Form layout="vertical">
+          <Form.Item label={<p className="text-white">Current Password</p>}>
+            <Input placeholder="******" />
+          </Form.Item>
+          <Form.Item label={<p className="text-white">New Password</p>}>
+            <Input placeholder="******" />
+          </Form.Item>
+          <Form.Item label={<p className="text-white">Confirm Password</p>}>
+            <Input placeholder="******" />
+          </Form.Item>
+          <div className="flex justify-center pb-10">
+            <button className="bg-[#22A59A] text-white p-2 rounded-sm">
+              Change Password
+            </button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
+};
 
-export default ChnagePassword
+export default ChnagePassword;
