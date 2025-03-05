97d40d2324e4436cca5febcb131a51a3551a14b6
shukumar-542 <shukumar542@gmail.com>
2025-03-05 10:31:02 +0600

    Implement privacy policy

--- src/Pages/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/Pages/PrivacyPolicy/PrivacyPolicy.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const PrivacyPolicy = () => {
   return (
-    <div>PrivacyPolicy</div>
+    <div className="text-white container mx-auto ">
+      <p className="text-center text-4xl font-semibold pt-10">Privacy Policy</p>
+      <p className="text-center mt-2">Home/Privacy Policy</p>
+
+      <div className="py-10">
+        <p className="text-xl font-semibold">1. Introduction</p>
+        <p className="mt-1">This Privacy Policy explains how we collect, use, and protect your personal information when you participate in our Refer & Earn Program. By using this program, you consent to the practices outlined here</p>
+        <p className=" mt-2">2. Information We Collect</p>
+        <ul className="mt-5 space-y-2">
+            <li>The program is open to registered users of our website.</li>
+            <li>Participants must be at least 18 years old.</li>
+            <li>Employees, affiliates, and partners may have restrictions on participation.</li>
+        </ul>
+
+        <p className="text-xl font-semibold mt-5">3. How It Works</p>
+        <ul className="mt-5 space-y-2">
+            <li>Users will receive a unique referral link.</li>
+            <li>When a new user signs up using the referral link and meets the qualifying conditions, the referrer will earn a reward.</li>
+            <li>Employees, affiliates, and partners may have restrictions on participation.</li>
+        </ul>
+        <p className="text-xl font-semibold mt-5">3. Reward System</p>
+        <ul className="mt-5 space-y-2">
+            <li>Users will receive a unique referral link.</li>
+            <li>When a new user signs up using the referral link and meets the qualifying conditions, the referrer will earn a reward.</li>
+            <li>Employees, affiliates, and partners may have restrictions on participation.</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
