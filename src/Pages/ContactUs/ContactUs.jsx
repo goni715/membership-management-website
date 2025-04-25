f652ced51a713e876b86f935df03b82e869c8fb9
naimuddin94 <developernaimuddin@gmail.com>
2025-04-25 12:21:34 +0600

    contact us functionality done

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -1,26 +1,24 @@
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoMdMail } from "react-icons/io";
-import { Input, Button } from "antd";
+import { Input } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router";
 
 const ContactUs = () => {
-  // Initialize React Hook Form
+  const navigate = useNavigate();
   const {
-    register,
+    control,
     handleSubmit,
     formState: { errors },
     reset,
   } = useForm();
 
-  // Handle form submission
   const onSubmit = async (data) => {
     try {
-      // Trigger Formspree form submission via POST request
       const response = await fetch(import.meta.env.VITE_FORMSPREE_ADDRESS, {
-        // Replace {form_id} with your actual form ID from Formspree
         method: "POST",
         body: JSON.stringify(data),
         headers: {
@@ -29,9 +27,9 @@ const ContactUs = () => {
       });
 
       if (response.ok) {
-        // Show success notification
-        toast.success("Message send successfully");
+        toast.success("Message sent successfully");
         reset();
+        navigate("/");
       } else {
         toast.error("Failed to send message");
       }
@@ -43,7 +41,7 @@ const ContactUs = () => {
   return (
     <div className="text-white container mx-auto">
       <p className="text-center text-4xl font-semibold pt-10">Contact Us</p>
-      <p className="text-center mt-2">Home/Contact Us</p>
+      <p className="text-center mt-2">Home / Contact Us</p>
       <div className="md:flex justify-between gap-36 mt-20 py-20">
         {/* Contact Details */}
         <div className="w-full">
@@ -66,18 +64,17 @@ const ContactUs = () => {
 
         {/* Contact Form */}
         <div className="w-full">
-          <form
-            onSubmit={handleSubmit(onSubmit)} // Trigger onSubmit from React Hook Form
-            action="https://formspree.io/f/{form_id}" // Replace {form_id} with your actual form ID from Formspree
-            method="POST"
-          >
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {/* Full Name */}
             <div className="mb-4">
               <label className="text-white text-lg">Full Name</label>
-              <Input
+              <Controller
                 name="fullName"
-                placeholder="Type Name"
-                size="large"
-                {...register("fullName", { required: "Full name is required" })}
+                control={control}
+                rules={{ required: "Full name is required" }}
+                render={({ field }) => (
+                  <Input {...field} size="large" placeholder="Type Name" />
+                )}
               />
               {errors.fullName && (
                 <p className="text-red-500 text-sm mt-2">
@@ -86,20 +83,27 @@ const ContactUs = () => {
               )}
             </div>
 
+            {/* Email */}
             <div className="mb-4">
               <label className="text-white text-lg">Email</label>
-              <Input
+              <Controller
                 name="email"
-                placeholder="Type Email"
-                size="large"
-                type="email"
-                {...register("email", {
+                control={control}
+                rules={{
                   required: "Email is required",
                   pattern: {
                     value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
                     message: "Please enter a valid email address",
                   },
-                })}
+                }}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    size="large"
+                    type="email"
+                    placeholder="Type Email"
+                  />
+                )}
               />
               {errors.email && (
                 <p className="text-red-500 text-sm mt-2">
@@ -108,14 +112,21 @@ const ContactUs = () => {
               )}
             </div>
 
+            {/* Phone Number */}
             <div className="mb-4">
               <label className="text-white text-lg">Phone Number</label>
-              <Input
+              <Controller
                 name="phone"
-                placeholder="Phone Number..."
-                size="large"
-                type="tel"
-                {...register("phone", { required: "Phone number is required" })}
+                control={control}
+                rules={{ required: "Phone number is required" }}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    size="large"
+                    type="tel"
+                    placeholder="Phone Number..."
+                  />
+                )}
               />
               {errors.phone && (
                 <p className="text-red-500 text-sm mt-2">
@@ -124,13 +135,16 @@ const ContactUs = () => {
               )}
             </div>
 
+            {/* Message */}
             <div className="mb-4">
               <label className="text-white text-lg">Message</label>
-              <TextArea
+              <Controller
                 name="message"
-                placeholder="Message..."
-                size="large"
-                {...register("message", { required: "Message is required" })}
+                control={control}
+                rules={{ required: "Message is required" }}
+                render={({ field }) => (
+                  <TextArea {...field} size="large" placeholder="Message..." />
+                )}
               />
               {errors.message && (
                 <p className="text-red-500 text-sm mt-2">
@@ -139,6 +153,7 @@ const ContactUs = () => {
               )}
             </div>
 
+            {/* Submit */}
             <button
               type="submit"
               className="bg-[#22A59A] w-full text-white rounded-sm py-2 text-xl cursor-pointer"
