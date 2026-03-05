77fc6f6972214c7bb0c08dcc2e6fd28b1f6c4891
goni715 <halesgoni715149123@gmail.com>
2026-03-05 14:53:33 +0600

    updated vite.config.jds

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import { createRequire } from 'module'
-const require = createRequire(import.meta.url)
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist",
   },
-  base: '/', 
+  base: "/",
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "src"),
     },
   },
-});                                                                                                                                                                                                                                                                                                                                                   
-
-
+});
